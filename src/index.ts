77fa93e44b7432cf0ac80bf2b376a4b08147df18
tusharnath10 <tusharnath10@gmail.com>
2023-06-18 05:10:24 +0530

    Refactoring

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import express from "express";
 import cors from "cors";
-import userRouter from "./src/routers/userRouter";
+import userRouter from "./routers/userRouter";
 
 const app = express();
 
