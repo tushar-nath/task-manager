2a06961cc1a06c26a76ccb19c9ff59c21845b497
tusharnath10 <tusharnath10@gmail.com>
2023-06-18 01:08:23 +0530

    minor refactoring

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,16 @@
 import express from "express";
+import cors from "cors";
 import userRouter from "./routers/userRouter";
 
 const app = express();
 
+// Enable CORS
+app.use(cors());
+
 app.use(express.json());
 app.use("/api/v1", userRouter);
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8000;
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
