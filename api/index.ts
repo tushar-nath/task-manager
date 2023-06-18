fa34bc25cfe7ca08a679bd863e089f9835459316
tusharnath10 <tusharnath10@gmail.com>
2023-06-18 14:25:03 +0530

    prod test

--- api/index.ts
+++ api/index.ts
@@ -1,6 +1,6 @@
 import express from "express";
 import cors from "cors";
-import userRouter from "./routers/userRouter";
+import userRouter from "../routers/userRouter";
 
 const app = express();
 
@@ -10,7 +10,7 @@ app.use(cors());
 app.use(express.json());
 app.use("/api/v1", userRouter);
 
-const port = process.env.PORT || 8000;
+const port = process.env.PORT || 8080;
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
